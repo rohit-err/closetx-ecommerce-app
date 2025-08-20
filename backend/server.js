e5e2dbb1ab1e470f03d8494e556d2d80a30d1d47
Rohit Kumar <rohit2004r82@gmail.com>
2025-08-21 05:00:58 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const paymentRoutes = require('./routes/paymentRoutes');
 const app = express()
 const port = process.env.PORT
 
-app.use(cors({ origin: ["http://localhost:5174", "http://localhost:5173"], credentials: true }))
+app.use(cors({ origin: ["http://localhost:5174", "http://localhost:5173","https://closetx-frontend.vercel.app"], credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/user", authRoutes)
