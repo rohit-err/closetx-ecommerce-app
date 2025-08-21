bda38b57b70e78d44ec5e6fcdf2f78819e35534b
Rohit Kumar <rohit2004r82@gmail.com>
2025-08-21 16:18:25 +0530

    fifth commit

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ const app = express()
 const port = process.env.PORT
 
 app.use(cors({ 
-    origin: true, 
+    origin: process.env.NODE_ENV === 'production' 
+        ? [process.env.FRONTEND_URL] 
+        : ["http://localhost:5174", "http://localhost:5173"], 
     credentials: true 
 }))
 app.use(express.json())
